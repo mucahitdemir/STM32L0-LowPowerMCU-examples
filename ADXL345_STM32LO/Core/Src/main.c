d238990a9c1d047afa1a3202e5e12a203ac1335e
mucahitdemir <43001724+mucahitdemir@users.noreply.github.com>
2022-08-13 12:38:24 +0300

    Update main.c

--- ADXL345_STM32LO/Core/Src/main.c
+++ ADXL345_STM32LO/Core/Src/main.c
@@ -1,20 +1,4 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
